539e0c2ceca1c71efc5aaa100d8b28460a4b6b10
PeterK85 <peter.ryan.kurtz@gmail.com>
2020-03-25 18:34:09 -0700

    Added scripts

---
+++ src/chili.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def init_tr(tr_num_str):
+    file_name = tr_num_str + "_task_report.tex"
+    tr_file = open(file_name, 'w')
+    tr_file.write("\\documentclass{article}\\usepackage[left=2cm,right=2cm,top=3cm]{geometry}\\usepackage[utf8]{inputenc}")
+    tr_file.write("\\setlength{\\parindent}{0pt}")
+    tr_file.write("\\usepackage{tabularx}")
+    tr_file.write("\\begin{document}")
+    return file_name
+
+def finish_the_chili(tr):
+    try:
+        subprocess.check_call(['pdflatex', tr])
+    except:
+        print("the chili went bad...")
---
+++ src/tr_test.py
@@ -0,0 +1,4 @@
+import chili
+
+file_name = chili.init_tr("85")
+chili.finish_the_chili(file_name)
